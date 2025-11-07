142beef47cbb6a1e673a12e8c1569290ed6fc55a
joao-enrique <almeidadesousajoaoenrique@gmail.com>
2025-11-07 14:37:41 -0300

    💾 Feat: update favicon and font, localize dialogue content to Portuguese

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,21 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  pc: `Este é o meu PC. Atualmente, trabalho principalmente com JavaScript/TypeScript.
+    Já fiz alguns jogos e aplicativos nessa linguagem. Também gosto de GDscript e Python. Enfim, independentemente da linguagem, eu simplesmente gosto de programar.
+    Aqui está o meu <a href="https://github.com/joao-enrique" target="_blank">Github</a>!`,
+  "cs-degree": `Este é o meu diploma de ensino médio-técnico. Eu o pendurei na parede porque tenho orgulho dele. Foi um curso bem teórico, mas acho que me deu uma boa base.`,
+  "sofa-table": `Esse é o meu sofá. Gosto de relaxar aqui e assistir ao YouTube.
+    Também faço devlogs dos meus projetos de jogos, sites e aplicativos no YouTube. Inscreva-se no <a href="https://www.youtube.com/@joaocodedev" target="_blank">meu canal</a>! (Se você gostar do conteúdo)`,
+  tv: `Essa é a minha TV. Ultimamente tenho assistido bastante a youtubers de tecnologia como:
+      <a href="https://www.youtube.com/@codehal" target="_blank">Codehal</a>, <a href="https://www.youtube.com/@cbtnuggets" target="_blank">CBT Nuggets</a>,
+      <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp.org </a> (Fiz esse resumo do github com eles)`,
+  bed: `É aqui que eu durmo. Grandes ideias surgem quando estou deitado na minha cama. Quando uma ideia me ocorre, muitas vezes preciso       anotá-la,senão não consigo dormir porque minha energia mental é consumida por ela.`,
+  resume: `Aqui é para ser meu curriculo mas como ainda estou criando ele,
+  me contate por email: almeidadsousajoaoenrique@gmail.com se você tem algum interesses de trabalho!`,
+  projects: `Informações sobre este portfólio: Ele foi feito com a biblioteca Kaboom.js, que é uma biblioteca para criar jogos em JavaScript.
+    O texto é renderizado com HTML/CSS. Portanto, a caixa de texto que você está lendo não é renderizada dentro do canvas.`,
+  library: `Tenho muitos livros de programação nas minhas estantes. Tem até um em inglês (aliás, eu também falo um pouco de inglês).
+    Provavelmente só li um deles. Quem mais compra livros técnicos compulsivamente sem nunca terminar de lê-los?`,
+  exit: `Se quiser sair só feche aba`,
 };
